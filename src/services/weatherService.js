46c62246f34a1d66ddc4e6a1dc170072be105cc6
Antonio-Savio <antonio.savio28@gmail.com>
2024-08-12 22:51:01 -0300

    delete logs

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,13 +5,11 @@ const APIkey = process.env.REACT_APP_API_KEY;
 export const fetchWeatherData = async (city, units) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIkey}`;
   const response = await axios.get(url);
-  console.log(response.data);
   return response.data;
 };
 
 export const fetchForecastData = async (lat, lon, units) => {
   const url_onecall = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=10&units=${units}&appid=${APIkey}`;
   const response = await axios.get(url_onecall);
-  // console.log(response.data);
   return response.data;
 };
